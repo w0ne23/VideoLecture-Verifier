f590e574e2bcb4190ee53ac37bdf6660d2d86365
w0ne23 <dawon081994@gmail.com>
2026-08-04 14:06:15 +0900

    fix: analyzer 모듈 shim이 private 헬퍼를 재수출 못하던 버그 수정

--- pipeline/analyzer/claim_extractor.py
+++ pipeline/analyzer/claim_extractor.py
@@ -1,1 +1,5 @@
 from pipeline.verifier.claim_extractor import *  # noqa: F401,F403
+from pipeline.verifier.claim_extractor import (  # noqa: F401
+    _claim_extract_batch_mode,
+    _claim_extract_context_window,
+)
